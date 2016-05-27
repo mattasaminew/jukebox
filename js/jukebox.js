01e9511ddd4be7f2ac7c666114e17adcdad40aa8
Alex Tjokro <alex@tjok.ro>
2016-05-26 17:06:03 -0700

    Update structure for HTML and JS file

---
+++ js/jukebox.js
@@ -0,0 +1,12 @@
+function parseNote(note) {
+	var noteArr = note.split("*");
+	// console.log(noteArr);
+	var noteObj = {};
+
+	for(i = 0; i < noteArr.length; i++) {
+		
+	}
+
+}
+
+parseNote("C#*2");
