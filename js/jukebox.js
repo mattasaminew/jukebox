b54cea8169a9aa6585d57a0a87077cb8a4fa56fa
Matthew Asaminew <mattasaminew@gmail.com>
2016-05-26 17:16:58 -0700

    Adding parseSong function

--- js/jukebox.js
+++ js/jukebox.js
@@ -4,9 +4,21 @@ function parseNote(note) {
 	var noteObj = {};
 
 	for(i = 0; i < noteArr.length; i++) {
-		
+
 	}
 
 }
-
 parseNote("C#*2");
+
+function parseSong(songString) {
+	var songArray = songString.split(" ");
+	var songLength = songArray.length;
+	var song = [];
+
+	var i = 0
+	while (i < songLength){
+		song.push(parseNote(songArray(i)));
+	}
+
+	return song;
+}
